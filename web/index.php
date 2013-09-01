30bc2ca6e2fdae18f9c13b520a9338d23bd442c0
blacknoir <deniskrasilnikov86@gmail.com>
2013-09-01 21:52:59 +0300

    index.php updated according to latest framework App component changes

--- web/index.php
+++ web/index.php
@@ -7,7 +7,7 @@ require_once '../vendor/autoload.php';
 try {
     /** @var Gears\Framework\App\App $app */
     $app = require_once '../vendor/gears-php/framework/bootstrap.php';
-    $app->run();
+    $app->init()->run();
 } catch (ResourceNotFound $e) {
     throw $e; // 404 page for prod environment
 }
